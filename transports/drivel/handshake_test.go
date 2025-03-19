8cffe8e5c3d1986da9095e3d6b63d5697c043ce7
Marc Himmelberger <marc.himmelberger@inf.ethz.ch>
2025-03-19 22:12:35 +0100

    Completed handshake benchmark

--- transports/drivel/handshake_test.go
+++ transports/drivel/handshake_test.go
@@ -238,12 +238,15 @@ func TestHandshakeDrivelcryptoServer(t *testing.T) {
 // observed on either the Client or Server is half the reported time per
 // operation since the benchmark does both sides.
 func BenchmarkHandshake(b *testing.B) {
+	kem := kemScheme
+	okem := okemScheme
+
 	// Generate the "long lasting" identity key and NodeId.
-	idKeypair, err := NewKeypair(false)
-	if err != nil || idKeypair == nil {
+	idKeypair := okem.KeyGen()
+	if idKeypair == nil {
 		b.Fatal("Failed to generate identity keypair")
 	}
-	nodeID, err := NewNodeID([]byte("\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13"))
+	nodeID, err := drivelcrypto.NewNodeID([]byte("\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13"))
 	if err != nil {
 		b.Fatal("Failed to load NodeId:", err)
 	}
@@ -251,38 +254,74 @@ func BenchmarkHandshake(b *testing.B) {
 
 	// Start the actual benchmark.
 	for i := 0; i < b.N; i++ {
-		// Generate the keypairs.
-		serverKeypair, err := NewKeypair(true)
-		if err != nil || serverKeypair == nil {
-			b.Fatal("Failed to generate server keypair")
-		}
-
-		clientKeypair, err := NewKeypair(true)
-		if err != nil || clientKeypair == nil {
+		// Generate the client keypair.
+		clientKeypair := kem.KeyGen()
+		if clientKeypair == nil {
 			b.Fatal("Failed to generate client keypair")
 		}
 
-		// Server handshake.
-		clientPublic := clientKeypair.Representative().ToPublic()
-		ok, serverSeed, serverAuth := ServerHandshake(clientPublic,
-			serverKeypair, idKeypair, nodeID)
-		if !ok || serverSeed == nil || serverAuth == nil {
-			b.Fatal("ServerHandshake failed")
+		// Client sends first message
+		clientHs := newClientHandshake(okem, kem, nodeID, idKeypair.Public(), clientKeypair)
+		msg1, err := clientHs.generateHandshake()
+		if err != nil {
+			b.Fatal("ClientHandshake failed", err)
+		}
+		if msg1 == nil {
+			b.Fatal("ClientHandshake is nil")
+		}
+		if len(msg1) < clientHs.lengthDetails.clientMinHandshakeLength {
+			b.Fatal("ClientHandshake is too short: %d bytes, expected at least %d",
+				len(msg1), clientHs.lengthDetails.clientMinHandshakeLength)
 		}
 
-		// Client handshake.
-		serverPublic := serverKeypair.Representative().ToPublic()
-		ok, clientSeed, clientAuth := ClientHandshake(clientKeypair,
-			serverPublic, idKeypair.Public(), nodeID)
-		if !ok || clientSeed == nil || clientAuth == nil {
-			b.Fatal("ClientHandshake failed")
+		// Server receives message
+		filter, err := replayfilter.New(replayTTL)
+		if err != nil {
+			b.Fatal("ServerHandshake failed to create replay filter", err)
+		}
+		serverHs := newServerHandshake(okem, kem, nodeID, idKeypair)
+		keySeedServer, err := serverHs.parseClientHandshake(filter, msg1)
+		if err != nil {
+			b.Fatal("ServerHandshake failed to parse", err)
+		}
+		if keySeedServer == nil {
+			b.Fatal("ServerHandshake derived nil KEY_SEED")
+		}
+		if len(keySeedServer) != drivelcrypto.KeySeedLength {
+			b.Fatal("ServerHandshake KEY_SEED is wrong length: %d bytes, expected %d",
+				len(keySeedServer), drivelcrypto.KeySeedLength)
+		}
+
+		// Server responds
+		msg2, err := serverHs.generateHandshake()
+		if err != nil {
+			b.Fatal("ServerHandshake failed", err)
+		}
+		if msg2 == nil {
+			b.Fatal("ServerHandshake is nil")
+		}
+		if len(msg2) < clientHs.lengthDetails.serverMinHandshakeLength {
+			b.Fatal("ServerHandshake is too short: %d bytes, expected at least %d",
+				len(msg2), clientHs.lengthDetails.serverMinHandshakeLength)
+		}
+
+		// Client receives
+		_, keySeedClient, err := clientHs.parseServerHandshake(msg2)
+		if err != nil {
+			b.Fatal("ClientHandshake failed to parse", err)
+		}
+		if keySeedClient == nil {
+			b.Fatal("ClientHandshake derived nil KEY_SEED")
+		}
+		if len(keySeedClient) != drivelcrypto.KeySeedLength {
+			b.Fatal("ClientHandshake KEY_SEED is wrong length: %d bytes, expected %d",
+				len(keySeedClient), drivelcrypto.KeySeedLength)
 		}
 
 		// Validate the authenticator.  Real code would pass the AUTH read off
 		// the network as a slice to CompareAuth here.
-		if !CompareAuth(clientAuth, serverAuth.Bytes()[:]) ||
-			!CompareAuth(serverAuth, clientAuth.Bytes()[:]) {
-			b.Fatal("AUTH mismatched between client/server")
+		if !bytes.Equal(keySeedServer, keySeedClient) {
+			b.Fatal("KEY_SEED mismatched between client/server")
 		}
 	}
 }
